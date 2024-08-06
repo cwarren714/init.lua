1cf3cce74bb105a11fd786cf67846e444b77514c
cwarren714 <chandlerawarren@gmail.com>
2024-08-06 18:50:02 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,14 +1,18 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable',
-  }
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
@@ -174,11 +178,11 @@ require('lazy').setup({
 
   -- Harpoon to quickly switch between docs
   { "ThePrimeagen/harpoon" },
-
-  -- codeium, will need to authorize with ':Codeium Auth'
   {
-    'Exafunction/codeium.vim',
-    event = 'BufEnter'
+      "supermaven-inc/supermaven-nvim",
+      config = function()
+        require("supermaven-nvim").setup({})
+      end,
   },
   -- simple markdown preview that opens in a browser
   {
