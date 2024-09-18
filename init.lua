3b30dd3f667b8baabda878b5e025ea85c905ad9b
cwarren714 <chandlerawarren@gmail.com>
2024-09-18 15:36:53 -0400

    adding changes for quickfix list nav

--- init.lua
+++ init.lua
@@ -296,6 +296,12 @@ vim.keymap.set({ "n", "v" }, "<leader>l", "<C-6>")
 -- trying to format doc on save
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
 
+--quickfix command shortcuts
+vim.keymap.set('n', '<leader>qn', ':cnext<CR>')
+vim.keymap.set('n', '<leader>qp', ':cprevious<CR>')
+vim.keymap.set('n', '<leader>qf', ':cfirst<CR>')
+vim.keymap.set('n', '<leader>ql', ':clast<CR>')
+
 -- toggle supermaven
 vim.keymap.set('n', '<leader>sm', function()
   local api = require('supermaven-nvim.api')
@@ -693,6 +699,32 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+function Remove_qf_item()
+  local curqfidx = vim.fn.line('.')
+  local qfall = vim.fn.getqflist()
+
+  -- Return if there are no items to remove
+  if #qfall == 0 then return end
+
+  -- Remove the item from the quickfix list
+  table.remove(qfall, curqfidx)
+  vim.fn.setqflist(qfall, 'r')
+
+  -- Reopen quickfix window to refresh the list
+  vim.cmd('copen')
+
+  -- If not at the end of the list, stay at the same index, otherwise, go one up.
+  local new_idx = curqfidx < #qfall and curqfidx or math.max(curqfidx - 1, 1)
+
+  -- Set the cursor position directly in the quickfix window
+  local winid = vim.fn.win_getid() -- Get the window ID of the quickfix window
+  vim.api.nvim_win_set_cursor(winid, { new_idx, 0 })
+end
+
+-- quickfix list delete keymap bound to "dd" when hovering over item in quickfix list
+vim.cmd("command! RemoveQFItem lua Remove_qf_item()")
+vim.api.nvim_command("autocmd FileType qf nnoremap <buffer> dd :RemoveQFItem<cr>")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
